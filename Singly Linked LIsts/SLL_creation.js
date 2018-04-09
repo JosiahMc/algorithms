e113c42ebdc4034eb2258babd099fac1cdff5571
Josiah McPeak <mcpeakjosiah@gmail.com>
2018-04-09 13:42:37 -0700

    Added random number and loop to add x amount of nodes between rand.x and y

--- Singly Linked LIsts/SLL_creation.js
+++ Singly Linked LIsts/SLL_creation.js
@@ -2,49 +2,41 @@ function Node(val) {
     this.val = val;
     this.next = null;
 }
+
+
 nn = new Node(15)
-console.log(nn)
-console.log(nn.val)
+// console.log(nn)
+// console.log(nn.val)
 
 function SinglyList() {
     this.head = null;
 }
 NewSLL = new SinglyList()
-console.log(NewSLL)
+// console.log(NewSLL)
 
 SinglyList.prototype.add = function(val) {
     var new_node = new Node(val)
     new_node.next = this.head
     this.head = new_node
 }
-SinglyList.prototype.add(35)
-SinglyList.prototype.add(34)
-SinglyList.prototype.add(123)
-SinglyList.prototype.add("whale")
-console.log(NewSLL)
-console.log(SinglyList)
-// SinglyList.prototype.add = function(value) {
-//     var node = new Node(value),
-//         currentNode = this.head;
- 
-//     // 1st use-case: an empty list 
-//     if (!currentNode) {
-//         this.head = node;
-//         this._length++;
-         
-//         return node;
-//     }
- 
-//     // 2nd use-case: a non-empty list
-//     while (currentNode.next) {
-//         currentNode = currentNode.next;
-//     }
- 
-//     currentNode.next = node;
- 
-//     this._length++;
-     
-//     return node;
-// };
 
-// console.log(SinglyList)
+SinglyList.prototype.print_data = function() {
+    current = this.head
+    while(current != null) {
+        console.log(current.val)
+        console.log(current.next)
+        current = current.next
+
+    }
+}
+
+for(var i = 0; i <= 5; i++) {
+    var random_num = Math.floor(Math.random() * 1000); 
+    SinglyList.prototype.add(random_num)
+
+}
+
+
+SinglyList.prototype.print_data()
+
+
